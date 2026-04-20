c03d59cf55281358f0dcff2d61d6c3380d655f7e
tomozo <tomozo-ozomot@hotmail.co.jp>
2026-04-20 23:20:23 +0900

    新規追加コード

---
+++ MyPDF/CsvBookmark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPDF
+{
+    internal class CsvBookmark
+    {
+        public string? Title;
+        public int Page;
+        public int Level;
+        public FontStyle Style;
+        public Color Color;
+    }
+}
