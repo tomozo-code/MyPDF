0b4ee374802d46585b21a7e84ac09f2eeb220dfa
tomozo <tomozo-ozomot@hotmail.co.jp>
2026-05-30 18:03:45 +0900

    PDF開く結果格納用

---
+++ MyPDF/PdfLoadResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// ==============================
+// PDF開く結果格納用
+// ==============================
+
+namespace MyPDF
+{
+    internal class PdfLoadResult
+    {
+        // 作業用ファイルパス用
+        public string WorkingPath { get; set; } = "";
+        // PDFの設定値
+        public PdfSettings Settings { get; set; } = null!;
+        // 表示用
+        public PdfiumViewer.PdfDocument Document { get; set; } = null!;
+    }
+}
